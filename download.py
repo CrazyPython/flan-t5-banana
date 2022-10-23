cbf4c2e4a448a381ae40bbf15099ccd8ba3b99a7
James <james@example.com>
2022-10-22 23:34:22 -0400

    Add google as vendor

--- download.py
+++ download.py
@@ -7,7 +7,7 @@ from transformers import pipeline
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    pipeline('text2text-generation', model='flan-t5-xxl')
+    pipeline('text2text-generation', model='google/flan-t5-xxl')
 
 if __name__ == "__main__":
     download_model()
