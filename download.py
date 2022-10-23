9a2258fb2cbb2cc7d9bdc9bc65da89de64bcecef
James <james@example.com>
2022-10-22 21:45:17 -0400

    Add predownload

--- download.py
+++ download.py
@@ -7,7 +7,7 @@ from transformers import pipeline
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    pipeline('fill-mask', model='bert-base-uncased')
+    pipeline('text2text-generation', model='flan-t5-xxl')
 
 if __name__ == "__main__":
     download_model()
