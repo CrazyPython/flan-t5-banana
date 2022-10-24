06f710e87845d02e8ec2f7054006a3deb2a8123f
James <james@example.com>
2022-10-24 03:36:08 -0400

    Switch to int8

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def init():
 
     device = 0 if torch.cuda.is_available() else -1
     tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-xxl")
-    model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-xxl", device=device, torch_dtype=torch.float16)
+    model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-xxl", device_map="auto", load_in_8bit=True)
 
     input_text = "translate English to German: How old are you?"
     input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to("cuda")
--- download.py
+++ download.py
@@ -7,7 +7,7 @@ def download_model():
     """You cannot use CUDA here, because the driver is not available during container build time."""
     # do a dry run of loading the huggingface model, which will download weights
     tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-xxl")
-    model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-xxl", torch_dtype=torch.float16)
+    model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-xxl", load_in_8bit=True)
 
 
 if __name__ == "__main__":
