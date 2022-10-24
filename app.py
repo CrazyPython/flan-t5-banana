475952bac70aa54d9ef59f21a8ff02822925693c
James <james@example.com>
2022-10-23 23:57:55 -0400

    Reformat

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 import torch
 
+
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
     global model, tokenizer
-    
+
     device = 0 if torch.cuda.is_available() else -1
     tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-xxl")
     model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-xxl", device=device, torch_dtype=torch.float16)
@@ -13,16 +14,17 @@ def init():
     input_text = "translate English to German: How old are you?"
     input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to("cuda")
 
+
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
-def inference(model_inputs:dict) -> dict:
+def inference(model_inputs: dict) -> dict:
     global model
 
     # Parse out your arguments
     prompt = model_inputs.get('prompt', None)
     if prompt == None:
         return {'message': "No prompt provided"}
-    
+
     # Run the model
     input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to('cuda')
     outputs = model.generate(input_ids)
--- download.py
+++ download.py
@@ -1,13 +1,10 @@
-# In this file, we define download_model
-# It runs during container build time to get model weights built into the container
-
-# In this example: A Huggingface BERT model
 import torch
 
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-"""You cannot use CUDA here, because the driver is not available during container build time."""
+
 def download_model():
+    """You cannot use CUDA here, because the driver is not available during container build time."""
     # do a dry run of loading the huggingface model, which will download weights
     tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-xxl")
     model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-xxl", torch_dtype=torch.float16)
