4f8eeb64e349eb02f64beb121f62a28ec78f111d
James <james@example.com>
2022-10-29 00:42:58 -0400

    Switch to -xl and use bfloat16

--- download.py
+++ download.py
@@ -6,8 +6,8 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 def download_model():
     """You cannot use CUDA here, because the driver is not available during container build time."""
     # do a dry run of loading the huggingface model, which will download weights
-    tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-xxl")
-    model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-xxl", device_map="auto", load_in_8bit=True)
+    tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-xl")
+    model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-xl")
 
 
 if __name__ == "__main__":
